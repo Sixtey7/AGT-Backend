72b3ba16bed32ca1e72c90f44ad855b0e37dd67d
Patrick Shaw <sixtey7@gmail.com>
2020-03-16 22:52:13 -0400

    start working on importing

--- routes/export_routes.py
+++ routes/export_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort
 import utils.Exporter as Exporter
 
 export_api = Blueprint('export_api', __name__)
@@ -13,3 +13,13 @@ def export_all():
     export_return = Exporter.export_all_data()
 
     return export_return, 200
+
+
+@export_api.route('', methods=['PUT'])
+def import_all():
+    """Imports all of the data into the database from a CSV file
+    """
+    if not request.json:
+        abort(400, 'No request body provided')
+
+    Exporter.import_all_date(request.json['input_string'])
--- utils/Exporter.py
+++ utils/Exporter.py
@@ -28,3 +28,11 @@ def export_all_data():
     print("Finished printing out everything")
 
     return return_string
+
+
+def import_all_date(input_string):
+    """Facilitates the importing of all data from a CSV format
+
+    :param input_string: A string containing the data to be imported
+    """
+    print('Got the input string %s' % input_string)
