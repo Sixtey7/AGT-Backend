6271b9be02bf1c2763fb104c14b01b0c7cdbd567
Patrick Shaw <sixtey7@gmail.com>
2020-03-21 22:50:03 -0400

    build an Item out of the values

--- utils/Exporter.py
+++ utils/Exporter.py
@@ -1,4 +1,6 @@
 import model.CategoryDB as CategoryDB
+from model.models import Event, Item
+from uuid import uuid4
 
 
 def export_all_data():
@@ -17,10 +19,8 @@ def export_all_data():
     for category in all_categories:
         for item in category.items:
             line = ','.join([category.name, item.name, item.current_value, str(item.goal_date)])
-            line += ',"'
             if len(item.events) > 0:
                 line += ';'.join([str(event.date) for event in item.events])
-            line += '"'
 
             return_string += line + '\n'
 
@@ -37,6 +37,29 @@ def import_all_date(input_string):
     """
     print('Got the input string %s' % input_string)
     all_lines = [s.strip() for s in input_string.splitlines()]
+    print('Got %d lines' % len(all_lines))
+
     for line in all_lines:
         all_rows = line.split(',')
-    print('Got %d lines' % len(all_lines))
+        print('found %d rows' % len(all_rows))
+        category_name = all_rows[0]
+        item_name = all_rows[1]
+        current_value = all_rows[2]
+        goal_date = all_rows[3]
+
+        # Split the event string into an array of event dates
+        item_id = uuid4()
+        # TODO: Need to figure out category ids
+        # TODO: Need to figure out item type
+        # TODO: Need to add in goal_value
+        new_item = Item(id=item_id, name=item_name, category_id=uuid4(),
+                        item_type='tracked_positive', current_value=current_value,
+                        goal_value='5')
+        print('built the item: ' + new_item.__str__())
+        event_array = []
+        if len(all_rows)==5:
+            events_string = all_rows[4]
+            if events_string != '':
+                event_array = events_string.split(';')
+
+
