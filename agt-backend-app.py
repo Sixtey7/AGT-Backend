7d5f9ae242a274c3a4c5de5c5cdc2a36f1eec299
Patrick Shaw <sixtey7@gmail.com>
2020-04-05 09:47:17 -0400

    make the database type configurable

--- agt-backend-app.py
+++ agt-backend-app.py
@@ -10,6 +10,27 @@ import os
 # create the flask app
 app = Flask(__name__)
 
+
+def init_sqlite(location=None):
+    print ('Standing up sqlite')
+
+    if 'DB_LOC' in os.environ:
+        location = os.environ['DB_LOC']
+    else
+        print('location not set, using default of ./model/')
+        location = 'model/'
+
+    db_path = 'sqlite:///' + location + '/agt-backend.db'
+    print('standing up sqlite with path: ' + db_path)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+    app.config['SQLALCHEMY_ECHO'] = True
+
+
+def init_postgres():
+    print('Standing up postgres')
+    # TODO
+
+
 # add in CORS support
 CORS(app, resources={r"/items/*": {"origins": "*"}})
 CORS(app, resources={r"/categories/*": {"origins": "*"}})
@@ -22,6 +43,15 @@ app.register_blueprint(event_api, url_prefix="/events/")
 app.register_blueprint(export_api, url_prefix="/export/")
 
 # SQLAlchemy config
+if 'DB_TYPE' in os.environ:
+    if os.environ['DB_TYPE'] == 'sqlite':
+        init_sqlite()
+    elif os.environ['DB_TYPE'] == 'postgres':
+        init_postgres()
+else:
+    print('no database was specified, assuming sqlite')
+    init_sqlite()
+
 # read in the location of the database
 if 'DB_LOC' in os.environ:
     db_location = os.environ['DB_LOC']
@@ -42,3 +72,6 @@ import model.models
 db.create_all()
 
 app.run(host='0.0.0.0', port=5000, debug=True)
+
+
+
